f6d2b0b2157a40c79d373cb9b4208703546b7055
Amanda Fernandes <124813517+amandagpf@users.noreply.github.com>
2023-03-13 15:22:46 -0300

    Commit criando "sobre"'
    Tirei o botão de modo dark e light pois estava dando bug, irei ajustar amanhã.
    Iniciei a section "sobre".

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const btnMobile = document.getElementById('mobile-btn');
 
 function toggleMenu(){
+    if(Event.type === 'touchstart') Event.preventDefault();
     const navegar = document.getElementById('navegar');
     navegar.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
-
-function toggleMode(){
-    const html = document.documentElement
-    html.classList.toggle('dark')
-    html.classList.toggle('light')
-}
+btnMobile.addEventListener('touchstart', toggleMenu);
